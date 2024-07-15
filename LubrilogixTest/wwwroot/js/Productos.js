73255355d12ed885953e7b7d60c7da6e4c97470b
Ricardo Diaz <admin@ticogadgets.onmicrosoft.com>
2024-07-15 11:48:19 -0600

    added font awesome + jquery datatables

---
+++ LubrilogixTest/wwwroot/js/Productos.js
@@ -0,0 +1,3 @@
+﻿$(document).ready(function () {
+    let table = new DataTable('#productosTable');
+});
--- LubrilogixTest/wwwroot/js/site.js
+++ LubrilogixTest/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.getElementById('toggleButton').addEventListener('click', function () {
+    var sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('collapsed');
+});
