e63ebb9039c794d7f627649b70402f5df69598f7
Ricardo Diaz Elizondo <ridia@microsoft.com>
2024-08-02 21:52:29 -0600

    changed style

--- LubrilogixTest/wwwroot/js/site.js
+++ LubrilogixTest/wwwroot/js/site.js
@@ -2,6 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// Add the 'collapsed' class on page load
+window.addEventListener('DOMContentLoaded', (event) => {
+    var sidebar = document.getElementById('sidebar');
+    sidebar.classList.add('collapsed');
+});
+
 document.getElementById('toggleButton').addEventListener('click', function () {
     var sidebar = document.getElementById('sidebar');
     sidebar.classList.toggle('collapsed');
